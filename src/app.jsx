492c312dae188066982cd51f7f8e8ace54150b4f
Piotr Klosinski <mybettingstuff@gmail.com>
2015-07-03 08:15:54 +0200

    [#5] add react-router

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,27 @@
 var $ = require('jquery');
 var React = require('react');
 window.React = React;
+var Router = require('react-router');
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
 var Bootstrap = require('react-bootstrap');
 var StoriesList = require('./stories_list.jsx');
+
+var Popular = React.createClass({
+  render: function() {
+    return(
+      <StoriesList source='https://fierce-gorge-1132.herokuapp.com/stories' />
+    )
+  }
+});
+
+var Recent = React.createClass({
+  render: function() {
+    return(
+      <StoriesList source='https://fierce-gorge-1132.herokuapp.com/stories/recent' />
+    )
+  }
+});
 
 var App = React.createClass({
   render: function() {
@@ -10,21 +29,27 @@ var App = React.createClass({
       <div>
         <Bootstrap.Navbar brand='React Workshop'>
           <Bootstrap.Nav>
-            <Bootstrap.NavItem eventKey={1} href='/popular' source='https://fierce-gorge-1132.herokuapp.com/stories'>Popular</Bootstrap.NavItem>
-            <Bootstrap.NavItem eventKey={2} href='/recent' source='https://fierce-gorge-1132.herokuapp.com/recent'>Recent</Bootstrap.NavItem>
+            <Bootstrap.NavItem eventKey={1} href='#/popular' source='https://fierce-gorge-1132.herokuapp.com/stories'>Popular</Bootstrap.NavItem>
+            <Bootstrap.NavItem eventKey={2} href='#/recent' source='https://fierce-gorge-1132.herokuapp.com/recent'>Recent</Bootstrap.NavItem>
           </Bootstrap.Nav>
         </Bootstrap.Navbar>
         <div className='storiesContainer'>
           <h1 className='text-center'>Stories:</h1>
           <br/>
-          <StoriesList source='https://fierce-gorge-1132.herokuapp.com/stories' />
+          <RouteHandler />
         </div>
       </div>
     );
   }
 });
 
-React.render(
-  <App />,
-  document.getElementById('content')
+var routes = (
+  <Route handler={App}>
+    <Route path='popular' handler={Popular} />
+    <Route path='recent' handler={Recent} />
+  </Route>
 );
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
